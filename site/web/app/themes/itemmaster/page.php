68481f4302196c39fd7c2eea53e75ae9d4f9bb8f
David Johnson <davidjohnson1297@gmail.com>
2016-10-26 16:10:23 -0700

    update to page.php to match live staging version

--- site/web/app/themes/itemmaster/page.php
+++ site/web/app/themes/itemmaster/page.php
@@ -4,27 +4,15 @@
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <section class="content">
 
-      <header class="header">
-
-        <h1 class="entry-title">
-
-          <?php the_title(); ?>
-
-        </h1>
-
-      </header>
-
-      <section class="entry-content">
-
-        <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+        <div class="row">
 
           <?php the_content(); ?>
 
-      </section>
+        </div>
 
-    </article>
+      </section>
 
     <?php endwhile; endif; ?>
 
